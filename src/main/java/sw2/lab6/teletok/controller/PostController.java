f281c267a3e001d1c00688ff20b3105103f90ec7
Sergio Muro <sergio.muroc@pucp.pe>
2020-06-29 20:21:27 -0500

    pregunta 3

--- src/main/java/sw2/lab6/teletok/controller/PostController.java
+++ src/main/java/sw2/lab6/teletok/controller/PostController.java
@@ -1,15 +1,37 @@
 package sw2.lab6.teletok.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import sw2.lab6.teletok.entity.Post;
+import sw2.lab6.teletok.repository.PostRepository;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.List;
 
 @RestController
 @CrossOrigin
 public class PostController {
 
-    @GetMapping(value = {"", "/"})
-    public String listPost(){
-        return "post/list";
+    @Autowired
+    PostRepository postRepository;
+
+    @GetMapping(value = "/ws/post/list", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity listarPosts(Post post, @RequestParam("query") String query) {
+
+        HashMap<String,Post> responseMap = new HashMap<>();
+        if(postRepository.buscadorPost(query)!=null){
+
+            return new ResponseEntity(responseMap, HttpStatus.OK);
+        }else{
+            return new ResponseEntity(responseMap, HttpStatus.OK);
+        }
+
     }
 
     @GetMapping("/post/new")
@@ -44,4 +66,17 @@ public class PostController {
     public String postLike() {
         return "";
     }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity gestionExcepcion(HttpServletRequest request) {
+
+        HashMap<String, Object> responseMap = new HashMap<>();
+        if (request.getMethod().equals("POST") || request.getMethod().equals("PUT")) {
+            responseMap.put("estado", "error");
+            responseMap.put("msg", "Debe enviar un post");
+        }
+        return new ResponseEntity(responseMap, HttpStatus.BAD_REQUEST);
+    }
+
+
 }
